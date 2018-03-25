e294227d99fc767735be51a88cc38188c2ab426e
Harsh Nandan <hnandan@scsolutions.com>
2018-03-25 15:14:15 -0700

    Updated the version of Keras

--- behavioral_clone.py
+++ behavioral_clone.py
@@ -48,17 +48,17 @@ from keras.layers import Conv2D, Lambda
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Conv2D(24, 5, 5, activation=None))
-model.add(Conv2D(36, 5, 5, activation=None))
-model.add(Conv2D(48, 3, 3, activation=None))
-model.add(Conv2D(64, 3, 3, activation=None))
+model.add(Conv2D(24, (5, 5), activation=None))
+# model.add(Conv2D(36, 5, 5, activation=None))
+# model.add(Conv2D(48, 3, 3, activation=None))
+# model.add(Conv2D(64, 3, 3, activation=None))
 model.add(Flatten( ))
 model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 train_log = model.fit(x=X_train, y=y_train, validation_split=0.2, 
-                            shuffle=True, nb_epoch=2, batch_size=64)
+                            shuffle=True, epochs=2, batch_size=64)
 
 model.save('model.h5')
 
